1fb21a4a1c266682b24c237b6f38315d4f45c8ba
cycleseven <unvexer@gmail.com>
2021-12-05 15:13:46 +0000

    Day 5: part 2

---
+++ 05/part_2.py
@@ -0,0 +1,163 @@
+import collections
+import pprint
+import sys
+
+## Utils
+
+def raise_bad_line_error(line):
+    print("Bad line encountered")
+    pprint.pprint(line)
+    raise ValueError("Line is neither horizontal, vertical or diagonal")
+
+def get_line_range_in_dimension(line, dim):
+    # dim is "x" or "y"
+    range_start = line[0][dim]
+
+    # If the second coord defining the line has a smaller x/y value,
+    # then the range is _decrementing_. Figure out the range_end and
+    # step accordingly. The range_end is always one more step beyond,
+    # the final value, and step is -1 if decrementing.
+    if line[0][dim] > line[1][dim]:
+        range_end = line[1][dim] - 1
+        step = -1
+    else:
+        range_end = line[1][dim] + 1
+        step = 1
+
+    return range(range_start, range_end, step)
+
+def is_vertical_line(line):
+    return line[0]['x'] == line[1]['x']
+
+def is_horizontal_line(line):
+    return line[0]['y'] == line[1]['y']
+
+def is_diagonal_line(line):
+    return abs(line[0]['y'] - line[1]['y']) == abs(line[0]['x'] - line[1]['x'])
+
+def get_vertical_extremes(line):
+    min_y = min(line[0]['y'], line[1]['y'])
+    max_y = max(line[0]['y'], line[1]['y'])
+
+    return min_y, max_y
+
+def get_horizontal_extremes(line):
+    min_x = min(line[0]['x'], line[1]['x'])
+    max_x = max(line[0]['x'], line[1]['x'])
+
+    return min_x, max_x
+
+"""Given a line (defined by its endpoints), return the full array of points that form the line.
+
+eg. [{ 'x': 1, 'y': 1 }, { 'x': 1, 'y': 3 }]
+    => [ { 'x': 1, 'y': 1 }, { 'x': 1, 'y': 2 }, { 'x': 1, 'y': 3 } ]
+"""
+def line_to_points(line):
+    if is_vertical_line(line):
+        min_y, max_y = get_vertical_extremes(line)
+
+        return [
+            { "x": line[0]['x'], "y": y }
+            for y in range(min_y, max_y + 1)
+        ]
+    elif is_horizontal_line(line):
+        min_x, max_x = get_horizontal_extremes(line)
+
+        return [
+            { "x": x, "y": line[0]['y'] }
+            for x in range(min_x, max_x + 1)
+        ]
+    elif is_diagonal_line(line):
+        x_range = get_line_range_in_dimension(line, 'x')
+        y_range = get_line_range_in_dimension(line, 'y')
+
+        points = [
+            { "x": x, "y": y }
+            for x, y in zip(x_range, y_range)
+        ]
+        return points
+
+    raise_bad_line_error(line)
+
+## Input parsing
+
+lines = [
+    [
+        {
+            "x": int(coord.split(',')[0]),
+            "y": int(coord.split(',')[1])
+        }
+        for coord
+        in input_line.rstrip().split(' -> ')
+    ]
+    for input_line in sys.stdin.readlines()
+]
+
+## The algorithm
+
+# The idea is to break each line into the points that make it up. Then, find out if any of those
+# points match up with a point that's already been encountered _once_ (and no more than once,
+# we don't want to double-count points with an overlap).
+#
+# This is mainly achieved by tracking previous lines via the previous_vent_points data structure.
+# It's a nested dict, where the root keys are x-coords of previously encountered points,
+# the nested keys are the y-coords, and the values are True/False.
+#
+# For example, if we encounter these points: [1,1], [1,2], [1,3], previous_vent_points looks like
+# this:
+#
+# {
+#   "1": {
+#     "1": False,
+#     "2": False,
+#     "3": False,
+#   }
+# }
+#
+# If later on, the point [1,2] is found to overlap another line, this is tracked with a "True"
+# value:
+#
+# {
+#   "1": {
+#     "1": False,
+#     "2": True,
+#     "3": False,
+#   }
+# }
+#
+# This allows O(1) lookup of whether any point has (a) appeared in a previous line, and (b) has
+# appeared in a previous overlap.
+
+previous_vent_points = {}
+num_overlapping_points = 0
+
+for line in lines:
+    if not (is_horizontal_line(line) or is_vertical_line(line) or is_diagonal_line(line)):
+        print('Found bad line', line)
+        continue
+
+    line_points = line_to_points(line)
+
+    for point in line_points:
+        if point['x'] in previous_vent_points and point['y'] in previous_vent_points[point['x']]:
+            # We know we've seen this point before. Was it already involved in an overlap?
+            # If not, this is a new overlapping point, so increment num_overlapping_points.
+            if not previous_vent_points[point['x']][point['y']]:
+                # Mark the vent point as "used", we already know it's an overlapping point and
+                # we don't count it again in later iterations.
+                previous_vent_points[point['x']][point['y']] = True
+                num_overlapping_points += 1
+
+            continue
+
+        # This vent point is one we haven't seen before. Track it in previous_vent_points.
+        # Initially set the value to False (since the vent point hasn't yet been
+        # detected as an overlapping point).
+        try:
+            previous_vent_points[point['x']].update({
+                point['y']: False
+            })
+        except KeyError:
+            previous_vent_points[point['x']] = { point['y']: False }
+
+print(num_overlapping_points)
